d7447643b79d6168c54ae606dd9a788fce34eb5f
Forrest Collman <forrest.collman@gmail.com>
2021-06-28 22:31:08 -0700

    making live query start option configurable

--- dash_connectivity_viewer/connectivity_table/__init__.py
+++ dash_connectivity_viewer/connectivity_table/__init__.py
@@ -5,6 +5,7 @@ from .layout import title, page_layout, app_layout
 from .external_stylesheets import external_stylesheets
 from ..common.dash_url_helper import setup
 import flask
+from functools import partial
 
 __version__ = "0.0.1"
 
@@ -14,7 +15,9 @@ def create_app(name=__name__, config={}, **kwargs):
         kwargs["external_stylesheets"] = external_stylesheets
     app = Dash(name, **kwargs)
     app.title = title
-    app.layout = app_layout
-    setup(app, page_layout=page_layout)
+    config_layout = partial(app_layout, config=config)
+    config_pl = partial(page_layout, config=config)
+    app.layout = config_layout
+    setup(app, page_layout=config_pl)
     register_callbacks(app, config)
     return app
--- run_connectivity_table.py
+++ run_connectivity_table.py
@@ -1,5 +1,5 @@
 from dash_connectivity_viewer.connectivity_table import create_app
 
 if __name__ == "__main__":
-    app = create_app()
+    app = create_app(config={"LIVE_VALUE_DEFAULT": 0})
     app.run_server(port=8050)
