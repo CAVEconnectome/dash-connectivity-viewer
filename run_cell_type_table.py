eb11afb8cf4595920efd29207fc78036ad531c82
Casey Schneider-Mizell <caseysm@gmail.com>
2022-01-23 19:35:31 -0500

    example configs

--- run_cell_type_table.py
+++ run_cell_type_table.py
@@ -1,11 +1,20 @@
 from dash_connectivity_viewer.cell_type_table import create_app
 
-config = {
-    # "datastack": "minnie65_phase3_v1",
+minnie_config = {
+    "datastack": "minnie65_phase3_v1`",
+    "server_address": "https://global.daf-apis.com",
+}
+
+flywire_config = {
     "datastack": "flywire_fafb_production",
     "server_address": "https://global.daf-apis.com",
 }
 
+fanc_config = {
+    "datastack": "fanc_production_mar2021",
+    "server_address": "https://global.daf-apis.com",
+}
+
 if __name__ == "__main__":
-    app = create_app(config=config)
+    app = create_app(config=minnie_config)
     app.run_server(port=8050)
--- run_connectivity_table.py
+++ run_connectivity_table.py
@@ -1,22 +1,25 @@
 from dash_connectivity_viewer.connectivity_table import create_app
 
-config = {
+
+minnie_config = {
+    "datastack": "minnie65_phase3_v1",
+    "server_address": "https://global.daf-apis.com",
+    "syn_position_column": "ctr_pt",
+}
+
+flywire_config = {
+    "datastack": "flywire_fafb_production",
+    "server_address": "https://global.daf-apis.com",
+    "syn_position_column": "pre_pt",
+}
+
+fanc_config = {
     "datastack": "fanc_production_mar2021",
     "server_address": "https://global.daf-apis.com",
     "syn_position_column": "pre_pt",
     "nucleus_table": "nuclei_aug2021ver2",
-    # "synapse_aggregation_rules": {
-    #     "mean_size": {
-    #         "column": "size",
-    #         "agg": "mean",
-    #     },
-    #     "net_size": {
-    #         "column": "size",
-    #         "agg": "sum",
-    #     },
-    # },
 }
 
 if __name__ == "__main__":
-    app = create_app(config=config)
+    app = create_app(config=fanc_config)
     app.run_server(port=8050)
